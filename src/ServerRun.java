9b0d44054c4f30d2ab8ef71ce8036cc66c406cfe
56Aang <a89588@alunos.uminho.pt>
2021-04-12 22:18:39 +0100

    fastSrv

--- src/ServerRun.java
+++ src/ServerRun.java
@@ -1,5 +1,8 @@
+import javax.xml.crypto.Data;
+import java.io.File;
 import java.io.IOException;
 import java.net.*;
+import java.nio.file.Files;
 
 /*
     Classe correspondende aos FastFileServers
@@ -21,21 +24,44 @@ class ServerRun {
         this.connectedServer = connectedServer;
     }
 
+    private void handleRequest(Packet fsChunk) {
+        try {
+            String path = "~/Files/";
+
+            File filePath = new File(path + fsChunk.getPayloadStr());
+
+            byte[] bytes = Files.readAllBytes(filePath.toPath());
+            byte[] buf = new byte[256];
+            Packet fsChunkPacket = new Packet(fsChunk.getPacketID(), this.address.getHostAddress() + ":" + 0 + ":" + this.port, 4, 0, bytes);
+
+            System.out.println("[10 DEBUG - handleRequest]:\n" + fsChunkPacket.toString());
+            buf = fsChunkPacket.packetToBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, this.address, this.port);
+
+            this.socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     public static void main(String[] args) throws SocketException, UnknownHostException {
         System.out.println(args[0]);
-        ServerRun sr = new ServerRun(args[0], InetAddress.getByName(args[1]),Integer.parseInt(args[2]));
+        ServerRun sr = new ServerRun(args[0], InetAddress.getByName(args[1]), Integer.parseInt(args[2]));
 
         Thread t = new Thread("aux") { // thread responsável por manter servidor vivo
             private DatagramSocket socket = sr.socket;
-            private byte[] buf = new Packet(-1, sr.address.getHostAddress() + ":" + 0 + ":" + sr.port, 2, 0, (sr.name+";"+sr.port).getBytes()).packetToBytes();
+            private byte[] buf = new Packet(-1, sr.address.getHostAddress() + ":" + 0 + ":" + sr.port, 2, 0, (sr.name + ";" + sr.port).getBytes()).packetToBytes();
             private DatagramPacket packet = new DatagramPacket(buf, buf.length, sr.connectedServer, sr.port);
+
             public void run() {
                 boolean running = true;
                 while (running) {
                     try {
                         socket.send(packet);
-                        sleep(10000);
+                        Thread.sleep(10000);
                     } catch (InterruptedException | IOException e) {
                         e.printStackTrace();
                         running = false;
@@ -49,12 +75,31 @@ class ServerRun {
         /*
          receber pedidos!!
          */
+        try {
 
+            boolean running = true;
+            while (running) {
+                byte[] buf = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                sr.socket.receive(packet);
+                System.out.println("[10] ServerRun:> Received connection from :" + packet.getAddress());
+                Packet fsChunk = new Packet(packet.getData());
+                switch (fsChunk.getType()) {
+                    case 5:
+                        sr.handleRequest(fsChunk);
+                        break;
+                    default:
+                        System.out.println("[10 DEBUG]Something went wrong:\n" +
+                                "[10 DEBUG]\n" + fsChunk.toString());
+                }
 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        {
 
-
-
-
+        }
 
 
         t.interrupt();
